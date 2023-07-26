4520b5a00bc3b7b01138ecb81bbe255b60d9cab2
server_42 <devbhattacharya42@gmail.com>
2023-07-26 21:20:11 +0530

    Create Functionaliy added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './components/Todo';
 import React, {useState,useEffect} from 'react'
 import {db} from './firebase'
-import {query,collection,onSnapshot,updateDoc,doc} from 'firebase/firestore'
+import {query,collection,onSnapshot,updateDoc,doc,addDoc} from 'firebase/firestore'
 let style = {
   bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
@@ -15,8 +15,26 @@ let style = {
 
 function App() {
   let [todos,setTodos] = useState([])
+  let [input,setInput] = useState('')
 
   // Create Todo
+  let createTodo = async(e) =>{
+    e.preventDefault(e)
+
+    if(input === ''){
+      alert('Please enter a valid TO-DO task!!!')
+      return
+    }
+    await addDoc(collection(db,'todos'),{
+      text: input,
+      completed: false,
+    })
+    // Below step is necessary done 
+    setInput('') 
+  }
+  // console.log(input)
+
+
   // Read Todo
   useEffect(()=>{
     const q = query(collection(db,'todos'))
@@ -43,8 +61,8 @@ function App() {
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
-        <form action="" className={style.form}>
-          <input type="text" placeholder="Add Todo" className={style.input}/>
+        <form onSubmit={createTodo} action="" className={style.form}>
+          <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Add Todo" className={style.input}/>
           <button className={style.button}><AiOutlinePlus size={30} /></button>
         </form>
         <ul>
@@ -52,7 +70,7 @@ function App() {
             <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
           ))}
         </ul>
-        <p className={style.count}>You have 2 todos</p> 
+        {todos.length < 1 ? <p className={style.count}>You have no tasks remaining</p> : <p className={style.count}>You have {todos.length} tasks remaining</p> }
       </div>
     </div>
   );
